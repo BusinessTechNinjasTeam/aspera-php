249d24299d9cb1150fefe63186ba8878b9e31da6
Kyle B. Johnson <kyle.johnson@hey.com>
2024-04-16 22:33:09 -0400

    wip

---
+++ README.md
@@ -0,0 +1,4 @@
+# Aspera on Cloud (AoC) PHP SDK
+
+- https://ibmaspera.com/help/registering_an_api_client
+- https://developer.ibm.com/apis/catalog/aspera--aspera-on-cloud-api/Authentication%20Setup%20for%20Aspera%20on%20Cloud%20APIs
---
+++ demo/index.php
@@ -0,0 +1,58 @@
+<?php
+
+$env = parse_ini_file('../.env');
+
+$privateKey = file_get_contents('../private.pem');
+$clientId = $env['CLIENT_ID'];
+$userEmail = $env['USER_EMAIL'];
+$orgSubdomain = $env['ORG_SUBDOMAIN'];
+$clientSecret = $env['CLIENT_SECRET'];
+
+$jwtHeader = [
+    'typ' => 'JWT',
+    'alg' => 'RS256'
+];
+
+$jwtBody = [
+    'iss' => $clientId,
+    'sub' => $userEmail,
+    'aud' => 'https://api.asperafiles.com/api/v1/oauth2/token',
+    'nbf' => time() - 3600,
+    'exp' => time() + 3600,
+];
+
+$payload = sprintf(
+    '%s.%s',
+    base64_encode(json_encode($jwtHeader)),
+    base64_encode(json_encode($jwtBody))
+);
+
+openssl_sign(
+    $payload,
+    $signature,
+    $privateKey,
+    OPENSSL_ALGO_SHA256
+);
+
+$jwt = sprintf('%s.%s', $payload, base64_encode($signature));
+
+$filesUrl = "https://api.ibmaspera.com/api/v1/oauth2/$orgSubdomain/token";
+
+$content = [
+    'assertion' => $jwt,
+    'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+    'scope' => 'admin:all',
+];
+
+$ch = curl_init($filesUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($content));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['content_type' => 'application/x-www-form-urlencoded']);
+curl_setopt($ch, CURLOPT_USERPWD, "$clientId:$clientSecret");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$decoded_response = json_decode($response, true);
+
+var_dump($decoded_response);
